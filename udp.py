62f6db41894f617096061668cd9fc4df9f3731fc
gYunTian <54625060+gYunTian@users.noreply.github.com>
2022-03-06 01:46:44 +0800

    simple udp flood

--- udp.py
+++ udp.py
@@ -1,35 +1,37 @@
 import socket
 import argparse
 import base64
+import time
+import sys
+import os
 
-parser = argparse.ArgumentParser(description='UDP benchmark cli')
-parser.add_argument('--server', type=str, help='specify the server ip')
-parser.add_argument('--port', type=str, help='specify the server port')
-parser.add_argument('--file', nargs='?', type=str, help='specify the file name (if any)', default=None)
-parser.add_argument('--encode', action='store_true', help='specify the file name (if any)')
+# parser = argparse.ArgumentParser(description='UDP benchmark cli')
+# parser.add_argument('--server', type=str, help='specify the server ip')
+# parser.add_argument('--port', type=str, help='specify the server port')
+# parser.add_argument('--file', nargs='?', type=str, help='specify the file name (if any)', default=None)
+# parser.add_argument('--encode', action='store_true', help='specify the file name (if any)')
 
-args = parser.parse_args()
-
-def process_file():
-  print("JELL")
-  with open("./data/video_preview_h264.mp4", "rb") as video:
-    text = base64.b64decode(video.read())
-    print(text)
-
-def main():
-  SERVER_IP = args.server
-  SERVER_PORT = args.port 
-  FILE = args.file if args.file else ""
-
-  process_file()
-
-
-  # IP_ADDR = socket.gethostbyname(socket.gethostname())
-  # PORT = 3000
-
-  # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-  #   s.bind((IP_ADDR, PORT))
-  #   server = (SERVER_IP, SERVER_PORT)
+# args = parser.parse_args()
 
 if __name__ == "__main__":
-  main()
+  UDP_PORT = 55681
+  
+  with open(os.path.join('./data/', 'video.mp4'), 'rb') as f:
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket
+    client_socket.settimeout(5.0)
+    serverIP = socket.gethostbyname(socket.gethostname()) # IP address of the server (current machine)
+
+    start = time.time()
+    message = f.read(4096)
+
+    print("sending: "+str(len(message)))
+    client_socket.sendto(message, (serverIP, UDP_PORT))
+    try:
+        data, server = client_socket.recvfrom(4096)
+        end = time.time()
+        elapsed = end - start
+        print("Received reply!")
+        print(len(data))
+    except socket.timeout:
+        print('REQUEST TIMED OUT')
