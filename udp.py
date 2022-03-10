4d54cfaba15378a8f6fd27290380529da1302025
gYunTian <54625060+gYunTian@users.noreply.github.com>
2022-03-11 00:17:42 +0800

    added go back N

--- udp.py
+++ udp.py
@@ -1,10 +1,5 @@
-import socket
-import time
-import os
-import struct
-import timeit
-import random
-from tabnanny import check
+import socket, time, os, struct, timeit, random, threading
+
 from utils import rbt, network
 from collections import deque
 from pathlib import Path
@@ -43,8 +38,9 @@ def no_frills_udp_client():
         except socket.timeout:
             print('REQUEST TIMED OUT')
 
-
-def packet_ordering_udp_client(MTU=1472):
+# measure additional time taken for packet resending
+# this will just mass resend
+def packet_resending_udp_client(MTU=1472):
     serverIP = socket.gethostbyname(socket.gethostname())
     input_path = os.path.join('./data/', 'test.file')
 
@@ -108,11 +104,12 @@ def packet_ordering_udp_client(MTU=1472):
                 checker += 1
         print(checker)
 
-# for experiment to suceed, we need to check that all packets are received
-# we dont request for resend, we just record lost packet % and time taken
-#
+# packet reodering
 
+# reassembling the packets
 
+# do for local and cloud
+# do for different file sizes
 def varying_mtu_udp_client():
     PORT = 7890
     MTUS = [128, 256, 512, 1024, 1472, 2048, 4096, 6144, 8192, 10240]
@@ -122,6 +119,7 @@ def varying_mtu_udp_client():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.connect((serverIP, PORT))
     s = struct.Struct('!II')
+    a = struct.Struct('!dd')
 
     size = Path(input_path).stat().st_size
     for mtu in MTUS:  # loop mtus
@@ -153,20 +151,130 @@ def varying_mtu_udp_client():
                 payload = f.read(mtu)
                 sock.send(payload)
             
-            flag = False
-            while not flag:
-                data = sock.recv(mtu, socket.MSG_PEEK)
-                if (data):
-                    flag = True
-                print("Experiment -", mtu, ": ended")
+            print("Experiment -", mtu,": finished transmission, awaiting results")
+            results = []
+            while True:
+              data, addr = sock.recvfrom(mtu)
+              results.append(data)
+              if (len(results) == 2): break
 
+            time_taken, percent_received = a.unpack(results[-1])
+            print(time_taken, percent_received)
+            # while not flag:
+            #     data = sock.recvfrom(mtu)
+            #     if (data):
+            #         print(data)
+            #         flag = True
+            
+            print("Experiment -", mtu, ": received results")
 
         print("Experiment -", mtu, ": reset for 15s")
         time.sleep(15)
+
+buffer = list()
+time_stamp = list()
+last_ack_sent = -1
+in_transit = 0
+retransmission_time = 0.2
+var_lock = threading.Lock()
+
+def go_back_N_sender(sock, total_packets, window_size):
+  global buffer
+  global last_ack_sent
+  global in_transit
+  global time_stamp
+
+  time_stamp = [None]*total_packets
+  try:
+    while (last_ack_sent + 1 < total_packets):
+      var_lock.acquire()
+      packetCount = last_ack_sent + in_transit + 1 # get current index of packet to send
+      if (in_transit < window_size and packetCount < total_packets): # if packets sent is less than window and index within range
+        sock.send(buffer[packetCount]) # send packet in current index
+        time_stamp[packetCount] = time.time() # start timer
+        in_transit += 1 # increment num packets sent
+      
+      if (in_transit > 0 and time.time() - time_stamp[last_ack_sent + 1] > retransmission_time): # check for timeout in earliest sent packet
+        print("Sequence Number:",last_ack_sent + 1, "timed out")
+        in_transit = 0 # if timed out, resend all in window
+      
+      var_lock.release()
+
+    print("Sent all packets")
+  except Exception as e:
+    print("error in sender")
+    print(e)
+
+def go_back_N_receiver(sock, total_packets, a):
+  global buffer
+  global last_ack_sent
+  global in_transit
+  global time_stamp
+
+  try:
+    while (last_ack_sent + 1 < total_packets):
+      if (in_transit > 0): # if packets have been sent
+        data, addr = sock.recvfrom(1472)
+        data = a.unpack(data) 
+        ack, seq = data[0], data[1]
+
+        var_lock.acquire()
+        if (ack): # if it is an acknowledgement for receipt of packet
+          if (last_ack_sent + 1 == seq):
+            last_ack_sent += 1
+            in_transit -= 1
+          else: in_transit = 0
+        else: # faulty packet
+          in_transit = 0
+        var_lock.release()
+      
+  except Exception as e:
+    print("error in receiver")
+    print(e)
+
+def go_back_N_udp_client():
+
+  PORT = 7890
+  serverIP = socket.gethostbyname(socket.gethostname())
+  input_path = os.path.join('./data/', 'test.file')
+
+  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  sock.connect((serverIP, PORT))
+  s = struct.Struct('!IHH')
+  a = struct.Struct('!II')
+
+  size = Path(input_path).stat().st_size 
+
+  for k in range(5,10):
+    with open(input_path, 'rb') as f:
+      total_packets = int(size/1472) + 1
+
+      for i in range(0, total_packets + 1):
+          payload = f.read(1472)
+          buffer.append(network.create_packet(s, i, payload, total_packets))
+      try:
+        start = time.time()
+        receiver_thread = threading.Thread(target = go_back_N_receiver, args = (sock, total_packets, a))
+        sender_thread = threading.Thread(target = go_back_N_sender, args = (sock, total_packets, k))
+        receiver_thread.start()
+        sender_thread.start()
+
+        receiver_thread.join()
+        sender_thread.join()
+      except Exception as e:
+        print("Error in main")
+        print(e)
+
+      print("Transfer complete: ",k)
+      print("Time taken:",time.time()-start)
+  
+  sock.close()
+
 
 
 
 if __name__ == "__main__":
     # no_frills_udp_client()
-    varying_mtu_udp_client()
+    # varying_mtu_udp_client()
     # packet_ordering_udp_client(1472)
+    go_back_N_udp_client()
--- udp_server.py
+++ udp_server.py
@@ -1,11 +1,13 @@
-from itertools import count
-import socket, struct, threading
-import logging
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%a, %d %b %Y %H:%M:%S', 
-filename='./logs/varying_mtu.log', filemode='w')
-
 from timeit import default_timer as timer
+import socket
+import struct
+import threading
+import logging
+from utils import network
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='./logs/varying_mtu.log', filemode='w')
+
 
 def base_udp_scenario():
     UDP_PORT = 55681
@@ -28,7 +30,10 @@ def reodering_udp_scenario():
         data, addr = sock.recvfrom(1472)
         print("received message", len(data))
 
+
 received_count = 0
+
+
 def receiver(sock, send_mtu):
     global received_count
     global flag
@@ -43,16 +48,18 @@ def receiver(sock, send_mtu):
             sock.settimeout(3600)
             flag = True
             break
-    
+
 # Do locally
 # do on cloud
+
+
 def varying_mtu_udp_scenario():
     global received_count
     UDP_PORT = 7890
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(('', UDP_PORT))
     serverIP = socket.gethostbyname(socket.gethostname())
-    
+    a = struct.Struct('!dd')
     s = struct.Struct('!II')
     TIMEOUT = 5
 
@@ -61,7 +68,7 @@ def varying_mtu_udp_scenario():
 
     while not mtu:
         print("Status: awaiting next experiment")
-        data, addr = sock.recvfrom(100, socket.MSG_PEEK)
+        data, addr = sock.recvfrom(1000, socket.MSG_PEEK)
 
         if (data):
             data = s.unpack(data)
@@ -73,27 +80,72 @@ def varying_mtu_udp_scenario():
         start = timer()
         print("Status: awaiting flood", addr)
         sock.settimeout(TIMEOUT)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, total_packets*send_mtu)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF,
+                        total_packets*send_mtu)
         flag = False
 
-        receiverThread = threading.Thread(target = receiver, args = (sock,send_mtu))
+        receiverThread = threading.Thread(
+            target=receiver, args=(sock, send_mtu))
         receiverThread.start()
-
         receiverThread.join()
-        
+
         end = timer()
         rtt = end - start
-        print("Total % packets received:", round((received_count/total_packets)*100,5), " - received:",received_count,"| total:",total_packets )
-        print("Total time:", rtt-TIMEOUT)
+        percent = round((received_count/total_packets)*100, 5)
+        time = round(rtt-TIMEOUT, 5)
+        print("Total % packets received:", percent, " - received:",
+              received_count, "| total:", total_packets)
+        print("Total time:", time)
         print("Status: experiment complete - ", send_mtu)
-        
-        logging.info('serverIP=%s|RTT=%s|received=%s|total=%s|percent=%s', serverIP, round(rtt-TIMEOUT,5), received_count, total_packets, 
-            round((received_count/total_packets)*100,5))
+
+        end_packet = a.pack(time, percent)
+        print(a.unpack(end_packet))
+        sock.sendto(end_packet, addr)
+
+        logging.info('serverIP=%s|RTT=%s|received=%s|total=%s|percent=%s', serverIP, round(rtt-TIMEOUT, 5), received_count, total_packets,
+                     round((received_count/total_packets)*100, 5))
         received_count = 0
-        sock.sendto("1".encode(), addr)     
+
     sock.close()
+
+
+def go_back_N_udp_server():
+    PORT = 7890
+    serverIP = socket.gethostbyname(socket.gethostname())
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('', PORT))
+    s = struct.Struct('!IHH')
+    a = struct.Struct('!II')
+    
+    previous_seq = -1
+    sock.settimeout(5)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF,
+                        10000000)
+    print("Go back N UDP Server Started")
+
+    while True:
+        try:
+            data, addr = sock.recvfrom(1500)
+            sequenceNum, checkSum, total_packets, data = network.dessemble_packet(data)
+            
+            if (sequenceNum == previous_seq + 1):
+                ack_packet = a.pack(1, sequenceNum)
+                sock.sendto(ack_packet, addr)
+                previous_seq = sequenceNum
+
+        except socket.timeout as e:
+            print("Status: have not received anything in 5 secs, ending")
+            sock.settimeout(3600)
+            flag = True
+            break
+    
+    sock.close()
+
+
 
 
 if __name__ == "__main__":
     # reodering_udp_scenario()
-    varying_mtu_udp_scenario()
+    # varying_mtu_udp_scenario()
+    go_back_N_udp_server()
